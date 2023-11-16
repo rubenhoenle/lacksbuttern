a9c768ccd87125ab0002cde8fa3dcf1dd6b8bd6e
Ruben Hoenle <git@hoenle.xyz>
2023-11-17 00:51:58 +0100

    feat(app): add go template skeleton

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"os"
+	"text/template"
+)
+
+type Message struct {
+	Name   string
+	Host   string
+	Time   string
+	Intact bool
+}
+
 func main() {
-	println("hello world")
+	messages := []Message{
+		{
+			Name:   "Johannes",
+			Host:   "lachs-buttern.jetzt",
+			Time:   "in 10 Sekunden",
+			Intact: true,
+		},
+		{
+			Name:   "Ruben",
+			Host:   "lack-saufen.jetzt",
+			Time:   "vor 5 Minuten",
+			Intact: true,
+		},
+	}
+	var tmplFile = "messages.tmpl"
+	tmpl, err := template.New(tmplFile).ParseFiles(tmplFile)
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, messages)
+	if err != nil {
+		panic(err)
+	}
 }
